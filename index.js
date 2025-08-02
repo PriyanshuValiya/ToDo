d050015330f1ccb22672f5766cce241939f79186
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2025-08-02 10:27:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
         </style>
       </head>
       <body>
-        <h2>ToDo List</h2>
+        <h2>New ToDo List</h2>
         <form action="/add" method="POST">
           <input type="text" name="todo" required />
           <button type="submit">Add</button>
